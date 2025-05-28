ef31b70490fd88ba6577e8ec27a549e2463332a1
leqso7 <osqelan1@gmail.com>
2025-05-29 00:11:18 +0400

    das

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -675,11 +675,11 @@ export default function TransactionsDashboard() {
       
       {/* Floating chat support button */}
       <div className="fixed bottom-6 right-6">
-        <button className="bg-[#6345ED] hover:bg-opacity-90 text-white w-14 h-14 rounded-full flex items-center justify-center shadow-lg">
+        <Link href="/my-chats" className="bg-[#6345ED] hover:bg-opacity-90 text-white w-14 h-14 rounded-full flex items-center justify-center shadow-lg">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
           </svg>
-        </button>
+        </Link>
       </div>
     </div>
   );
